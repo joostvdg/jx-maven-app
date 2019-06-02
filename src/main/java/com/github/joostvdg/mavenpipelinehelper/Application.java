58c1daf300b19e067c874d377af739e47fd8d3fd
Joost van der Griendt <joostvdg@gmail.com>
2019-06-03 00:37:31 +0200

    Draft create

---
+++ src/main/java/com/github/joostvdg/mavenpipelinehelper/Application.java
@@ -0,0 +1,10 @@
+package com.github.joostvdg.mavenpipelinehelper;
+
+import io.micronaut.runtime.Micronaut;
+
+public class Application {
+
+    public static void main(String[] args) {
+        Micronaut.run(Application.class);
+    }
+}
---
+++ src/test/java/com/github/joostvdg/mavenpipelinehelper/MavenPipelineHelperCommandTest.java
@@ -0,0 +1,28 @@
+package com.github.joostvdg.mavenpipelinehelper;
+
+import io.micronaut.configuration.picocli.PicocliRunner;
+import io.micronaut.context.ApplicationContext;
+import io.micronaut.context.env.Environment;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class MavenPipelineHelperCommandTest {
+
+    @Test
+    public void testWithCommandLineOption() throws Exception {
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(baos));
+
+        try (ApplicationContext ctx = ApplicationContext.run(Environment.CLI, Environment.TEST)) {
+            String[] args = new String[] { "-v" };
+            PicocliRunner.run(MavenPipelineHelperCommand.class, ctx, args);
+
+            // maven-pipeline-helper
+            assertTrue(baos.toString().contains("Hi!"));
+        }
+    }
+}
